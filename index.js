6c151e1a2180f37d8d697e9b7adea3504bafef52
Charlie <ccariste@gmail.com>
2018-03-07 20:21:21 -0800

    CRC: Travelers works.  Not sure about setInterval use?

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 let orderNum = 0;
+
 let chgOrder = (e) => {
     e.target.style.order = orderNum--;
 };
@@ -15,3 +16,23 @@ red.addEventListener("click", chgOrder);
 blue.addEventListener("click", chgOrder);
 green.addEventListener("click", chgOrder);
 gray.addEventListener("click", chgOrder);
+
+
+let marginLeft = (e) => {
+    e.target.style = 'margin-left: 400%'
+};
+
+let deleteMl = (e) => {
+    e.target.style = 'margin-left: 10px'
+};
+
+pink.addEventListener("mousedown", marginLeft);
+pink.addEventListener("mouseup", deleteMl);
+red.addEventListener("mousedown", marginLeft);
+red.addEventListener("mouseup", deleteMl);
+blue.addEventListener("mousedown", marginLeft);
+blue.addEventListener("mouseup", deleteMl);
+green.addEventListener("mousedown", marginLeft);
+green.addEventListener("mouseup", deleteMl);
+gray.addEventListener("mousedown", marginLeft);
+gray.addEventListener("mouseup", deleteMl);
